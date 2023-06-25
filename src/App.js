d5ef20f7d54aef222ec8e556b3769a78591b8d3a
sameerkhawaja <sameerkhawaja01@gmail.com>
2023-06-25 19:48:31 -0400

    add gallery component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import About from './components/About';
-import Nav from './components/Nav'
-// import './App.css';
+import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
   return(
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
